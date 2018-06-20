8810ff21f6098df3fa420a4e8ea506ae7fbc2884
nadira.nuri <nadira.nuri@gmail.com>
2018-06-21 00:46:38 +0600

    t 6 20180621

--- www/static/my.js
+++ www/static/my.js
@@ -306,7 +306,7 @@ function clientList() {
 function shipTo() {	
 		var clientID=localStorage.clientID
 		clientID=clientID.replace(' ','')
-		var str=encodeURI(localStorage.cid) + '/' + localStorage.clientID + '/' + encodeURI(localStorage.userid) + '/' + encodeURI(localStorage.password) + '/' + encodeURI(localStorage.synccode)
+		var str=encodeURI(localStorage.cid) + '/' + clientID + '/' + encodeURI(localStorage.userid) + '/' + encodeURI(localStorage.password) + '/' + encodeURI(localStorage.synccode)
 		alert (apipath_shipto+"syncRep/"+str)
 		
 		$("#shiptoText").val(apipath_shipto+"syncRep/"+str);
