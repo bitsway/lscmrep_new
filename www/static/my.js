34e41d67f3b50f8d2652f0f7b83a1a071b287dfb
nadira.nuri <nadira.nuri@gmail.com>
2018-06-21 12:17:57 +0600

    t 9 20180621

--- www/static/my.js
+++ www/static/my.js
@@ -316,7 +316,7 @@ function shipTo() {
 				 success: function(result) {
 					
 						 loginResult=result
-						alert (loginResult)
+						
 						if (loginResult==''){
 							
 							$("#mySyncError").html('Error: 10002 Network Time out');
@@ -328,7 +328,7 @@ function shipTo() {
 						if ((loginResult!='Failed') & (loginResult!='')){
 							
 							var shipto = loginResult.split('</MREPSYNC>').replace('<MREPSYNC>','');			
-
+							alert (shipto)
 									localStorage.shipto=shipto;
 									alert (localStorage.shipto)
 									$('#shiptoCombo').empty();
